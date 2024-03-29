b25576b261cbde1ea80a89411cdd100f8490600e
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-03-29 17:38:09 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@ import router from './router';
 <template>
   <header>
     <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link> 
+    <router-link to="/categorias">Categorias</router-link>
   </header>
   <main>
     <RouterView />
